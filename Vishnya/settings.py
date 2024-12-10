875929dc41c8838d8024d053186da823d5e38178
6i3ek3ua1 <slyantyaev20@yandex.ru>
2024-12-10 03:50:58 +0300

    (((

--- Vishnya/settings.py
+++ Vishnya/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'iumrlqv^6(@g#n3jbcml!a^-7e-atj&zdws8j)%&h+uass7y4l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
